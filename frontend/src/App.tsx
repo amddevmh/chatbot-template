53ee19086cae0f51301ebc2a204c03f481a36795
a.mouelhi <100707670+amddevmh@users.noreply.github.com>
2025-03-13 22:31:14 +0100

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,7 +22,6 @@ export default function App() {
           <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
-            
           </header>
           <div className="flex flex-1 flex-col gap-4 p-4">
           
